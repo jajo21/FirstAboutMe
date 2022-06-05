bc68fe14b39a1e4f2a9248904bfcd3bfb801eff4
Johannes Jakobsson <jajo21pg@student.ju.se>
2022-06-06 00:57:41 +0200

    Sorterar repos samt lägger till info

--- script/link-script.js
+++ script/link-script.js
@@ -2,26 +2,14 @@ const getGithubData = async () => {
     const response = await fetch("https://api.github.com/users/jajo21/repos");
     const data = await response.json();
     console.log("Github API", data);
+    data.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+
     let githubRepos = document.querySelector(".githubRepos");
 
     for(i=0; i<data.length; i++) {
         githubRepos.insertAdjacentHTML("beforeend", "<li><a href='" + data[i].html_url + 
-        "' target='_blank'><i class='fab fa-github'></i></a>&emsp;&emsp;" + data[i].description);
-        
-        //+ "&emsp;&emsp;Skapad: " + data[i].created_at);
-
-
-        //Annat sätt att göra samma kod på. Men fastnade lite på mellanrummen &emsp;&emsp; med koden nedanför
-       /*  let listItem = document.createElement("li");
-        let link = document.createElement("a");
-        githubRepos.appendChild(listItem);
-        listItem.appendChild(link);
-        link.setAttribute("href", data[i].html_url);
-        link.setAttribute("target", "_blank");
-        link.innerHTML = '<i class="fab fa-github"></i>'; //&emsp;&emsp;' + data[i].description;
-        let insertDesc = document.createTextNode(data[i].description);
-        listItem.appendChild(insertDesc); */
-
+        "' target='_blank'><i class='fab fa-github'></i></a>&emsp;&emsp;" + data[i].name + 
+        '</br> <p>Närmare förklaring av repo: ' + data[i].description + '</p>');
     }
 }
 getGithubData();
